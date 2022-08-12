d14cede7d6e4a6479252d4a9cb168e4855b14f06
bsolis6878 <bsolis6878@gmail.com>
2022-08-11 19:11:17 -0500

    added datepicker when creating and editing tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,12 +101,21 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 0,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 })
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -240,6 +249,10 @@ $("#trash").droppable({
     console.log("out");
   }
 })
+
+$("#modalDueDate").datepicker({
+  minDate: 0
+});
 
 // load tasks for the first time
 loadTasks();
